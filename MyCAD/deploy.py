585a73667219f7449a3db4b9c2a420d453a81860
Alexey <kalinichenkoae@gmail.com>
2022-08-03 12:17:39 +0300

    Update deploy.py

--- MyCAD/deploy.py
+++ MyCAD/deploy.py
@@ -1,11 +1,13 @@
 development = {
 	'url': 'http://127.0.0.1:8000/main/',
+	'core_path': './core/build/'
 }
 
 production = {
 	'url': 'https://alexeymycad.pythonanywhere.com/main/',
+	'core_path': '/home/AlexeyMyCAD/MyCAD/core/build/'
 }
 
 # development if local
 # production if internet
-deploy = production
+deploy = development
