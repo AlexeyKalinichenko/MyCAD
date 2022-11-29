33722ce23094da1877e22b908f456554b7f58573
Alexey <kalinichenkoae@gmail.com>
2022-11-29 20:19:47 +0300

    Update server API

--- main/urls.py
+++ main/urls.py
@@ -6,22 +6,48 @@ urlpatterns = [
 
     path('OpenDocument/', views.OpenDocument, name='OpenDocument'),
     path('CloseDocument/', views.CloseDocument, name='CloseDocument'),
-    path('RunOperation/<int:operationId>/', views.RunOperation, name='RunOperation'),
 
-    path('ButtonEvent/<int:buttonId>/<int:state>/', views.ButtonEvent, name='ButtonEvent'),
-    path('MouseMoveEvent/<int:x>/<int:y>/', views.MouseMoveEvent, name='MouseMoveEvent'),
+    path('SetStyleData/', views.SetStyleData, name='SetStyleData'),
 
-    path('SendIntData/<int:value>/', views.SendIntData, name='SendIntData'),
-    path('SendStringData/<str:value>/', views.SendStringData, name='SendStringData'),
+    path('GetRenderingData/<int:docId>/', views.GetRenderingData, name='GetRenderingData'),
 
-    path('IsDocumentChanged/', views.IsDocumentChanged, name='IsDocumentChanged'),
-    path('GetDataForRendering/', views.GetDataForRendering, name='GetDataForRendering'),
-    path('GetDocumentInfo/', views.GetDocumentInfo, name='GetDocumentInfo'),
+    path('Undo/<int:docId>/', views.Undo, name='Undo'),
+    path('Redo/<int:docId>/', views.Redo, name='Redo'),
+    path('Commit/<int:docId>/', views.Commit, name='Commit'),
 
-    # Old API #
-    path('getdata/', views.getSceneData, name='getSceneData'),
-    path('setdata/', views.setSceneData, name='setSceneData'),
-    path('clearScene/', views.clearSceneData, name='clearSceneData'),
-    path('getStatistics/', views.getStatistics, name='getStatistics')
-    # Old API #
+    path('CreateLine/', views.CreateLine, name='CreateLine'),
+    path('EditLine/', views.EditLine, name='EditLine'),
+    path('DeleteLine/', views.DeleteLine, name='DeleteLine'),
+
+    path('GetLineNode/<int:docId>/<int:objId>/<int:index>/', views.GetLineNode, name='GetLineNode'),
+    path('GetLineLength/<int:docId>/<int:objId>/', views.GetLineLength, name='GetLineLength'),
+    path('GetLineAngle/<int:docId>/<int:objId>/', views.GetLineAngle, name='GetLineAngle'),
+    path('IsLineUnderCursor/', views.IsLineUnderCursor, name='IsLineUnderCursor'),
+
+    path('GetAllObjects/<int:docId>/', views.GetAllObjects, name='GetAllObjects'),
+    path('HighlightObject/<int:docId>/<int:objId>/', views.HighlightObject, name='HighlightObject'),
+
+
+    # Old1 API #
+    #path('OpenDocument/', views.OpenDocument, name='OpenDocument'),
+    #path('CloseDocument/', views.CloseDocument, name='CloseDocument'),
+    #path('RunOperation/<int:operationId>/', views.RunOperation, name='RunOperation'),
+
+    #path('ButtonEvent/<int:buttonId>/<int:state>/', views.ButtonEvent, name='ButtonEvent'),
+    #path('MouseMoveEvent/<int:x>/<int:y>/', views.MouseMoveEvent, name='MouseMoveEvent'),
+
+    #path('SendIntData/<int:value>/', views.SendIntData, name='SendIntData'),
+    #path('SendStringData/<str:value>/', views.SendStringData, name='SendStringData'),
+
+    #path('IsDocumentChanged/', views.IsDocumentChanged, name='IsDocumentChanged'),
+    #path('GetDataForRendering/', views.GetDataForRendering, name='GetDataForRendering'),
+    #path('GetDocumentInfo/', views.GetDocumentInfo, name='GetDocumentInfo'),
+    # Old1 API #
+
+    # Old2 API #
+    #path('getdata/', views.getSceneData, name='getSceneData'),
+    #path('setdata/', views.setSceneData, name='setSceneData'),
+    #path('clearScene/', views.clearSceneData, name='clearSceneData'),
+    #path('getStatistics/', views.getStatistics, name='getStatistics')
+    # Old2 API #
 ]
--- main/views.py
+++ main/views.py
@@ -19,171 +19,219 @@ def index(request):
 	return HttpResponse(template.render(context, request))
 
 def OpenDocument(request):
-	response = JsonResponse({ "result": 0 })
-	return response
+	pass
 
 def CloseDocument(request):
-	response = JsonResponse({ "result": 0 })
-	return response
+	pass
 
-def RunOperation(request, operationId):
-	response = JsonResponse({ "result": 0 })
-	return response
+def SetStyleData(request):
+	pass
 
-def ButtonEvent(request, buttonId, state):
-	response = JsonResponse({ "result": 0 })
-	return response
+def GetRenderingData(request, docId):
+	pass
 
-def MouseMoveEvent(request, x, y):
-	response = JsonResponse({ "result": 0 })
-	return response
+def Undo(request, docId):
+	pass
 
-def SendIntData(request, value):
-	response = JsonResponse({ "result": 0 })
-	return response
+def Redo(request, docId):
+	pass
 
-def SendStringData(request, value):
-	response = JsonResponse({ "result": 0 })
-	return response
+def Commit(request, docId):
+	pass
 
-def IsDocumentChanged(request):
-	response = JsonResponse({ "result": 0, "isChanged": True })
-	return response
+def CreateLine(request):
+	pass
 
-def GetDataForRendering(request):
-	response = JsonResponse({
-		"result": 0,
-		"buffer": {
-			"Indices": [
-        		{ "Figure": "triangles", "Offset": 0, "Count": 6 },	# need change count
-        		{ "Figure": "triangles", "Offset": 0, "Count": 6 },	# need change count
-        		{ "Figure": "points", "Offset": 18, "Count": 6 }	# need change count
-    		],
-    		"ObjectsColor": { "R": 0.85, "G": 0.87, "B": 0.91 },
-    		"HighlightColor": { "R": 0.98, "G": 0.68, "B": 0.35 },
-    		"NodesColor": { "R": 0.98, "G": 0.68, "B": 0.35 },
-			"Thickness": 0.01,
-			"NodesMode": False,
-    		"Vertices": [
-        		{ "X": -0.4, "Y": 0.4012, "Z": 0.0 },
-        		{ "X":  0.4, "Y":    0.4, "Z": 0.0 },
-        		{ "X": -0.4, "Y":    0.4, "Z": 0.0 },
-        		{ "X":  0.4, "Y":    0.4, "Z": 0.0 },
-        		{ "X":  0.4, "Y": 0.4012, "Z": 0.0 },
-        		{ "X": -0.4, "Y": 0.4012, "Z": 0.0 },
-        		{ "X": -0.4, "Y": 0.3025, "Z": 0.0 },
-        		{ "X":  0.4, "Y":    0.3, "Z": 0.0 },
-        		{ "X": -0.4, "Y":    0.3, "Z": 0.0 },
-        		{ "X":  0.4, "Y":    0.3, "Z": 0.0 },
-        		{ "X":  0.4, "Y": 0.3025, "Z": 0.0 },
-        		{ "X": -0.4, "Y": 0.3025, "Z": 0.0 },
-        		{ "X": -0.4, "Y":  0.205, "Z": 0.0 },
-        		{ "X":  0.4, "Y":    0.2, "Z": 0.0 },
-        		{ "X": -0.4, "Y":    0.2, "Z": 0.0 },
-        		{ "X":  0.4, "Y":    0.2, "Z": 0.0 },
-        		{ "X":  0.4, "Y":  0.205, "Z": 0.0 },
-        		{ "X": -0.4, "Y":  0.205, "Z": 0.0 },
-        		{ "X":  0.4, "Y": 0.4006, "Z": 0.0 },
-        		{ "X": -0.4, "Y": 0.4006, "Z": 0.0 },
-        		{ "X":  0.4, "Y": 0.3012, "Z": 0.0 },
-        		{ "X": -0.4, "Y": 0.3012, "Z": 0.0 },
-        		{ "X":  0.4, "Y": 0.2025, "Z": 0.0 },
-        		{ "X": -0.4, "Y": 0.2025, "Z": 0.0 } 
-    		]
-		}
-	})
-	return response
+def EditLine(request):
+	pass
 
-def GetDocumentInfo(request):
-	response = JsonResponse({
-		"result": 0,
-		"info": {
-			"colorTheme": "dark",
-			"thickness": "two",
-			"nodeMode": "off",
-			"objects": 123
-		}
-	})
-	return response
+def DeleteLine(request):
+	pass
 
-# Old API #
-def getSceneData(request):
-	filePath = os.path.join(os.getcwd(), 'data', 'data.json')
+def GetLineNode(request, docId, objId, index):
+	pass
 
-	f = open(filePath, "r")
-	jsonData = f.read()
-	f.close()
+def GetLineLength(request, docId, objId):
+	pass
 
-	if len(jsonData) != 0:
-		data = json.loads(jsonData)
-		vertices = data['vertices']
-		indices = data['indices']
-		counter = data['counter']
-	else:
-		vertices = []
-		indices = []
-		counter = 0
+def GetLineAngle(request, docId, objId):
+	pass
 
-	response = JsonResponse({
-			'vertices': vertices,
-			'indices': indices,
-			'counter': counter
-		})
+def IsLineUnderCursor(request):
+	pass
 
-	return response
+def GetAllObjects(request, docId):
+	pass
 
+def HighlightObject(request, docId, objId):
+	pass
+
+################
+
+#def OpenDocument(request):
+#	response = JsonResponse({ "result": 0 })
+#	return response
+#
+#def CloseDocument(request):
+#	response = JsonResponse({ "result": 0 })
+#	return response
+#
+#def RunOperation(request, operationId):
+#	response = JsonResponse({ "result": 0 })
+#	return response
+#
+#def ButtonEvent(request, buttonId, state):
+#	response = JsonResponse({ "result": 0 })
+#	return response
+#
+#def MouseMoveEvent(request, x, y):
+#	response = JsonResponse({ "result": 0 })
+#	return response
+#
+#def SendIntData(request, value):
+#	response = JsonResponse({ "result": 0 })
+#	return response
+#
+#def SendStringData(request, value):
+#	response = JsonResponse({ "result": 0 })
+#	return response
+#
+#def IsDocumentChanged(request):
+#	response = JsonResponse({ "result": 0, "isChanged": True })
+#	return response
+#
+#def GetDataForRendering(request):
+#	response = JsonResponse({
+#		"result": 0,
+#		"buffer": {
+#			"Indices": [
+#        		{ "Figure": "triangles", "Offset": 0, "Count": 6 },	# need change count
+#        		{ "Figure": "triangles", "Offset": 0, "Count": 6 },	# need change count
+#        		{ "Figure": "points", "Offset": 18, "Count": 6 }	# need change count
+#    		],
+#    		"ObjectsColor": { "R": 0.85, "G": 0.87, "B": 0.91 },
+#    		"HighlightColor": { "R": 0.98, "G": 0.68, "B": 0.35 },
+#    		"NodesColor": { "R": 0.98, "G": 0.68, "B": 0.35 },
+#			"Thickness": 0.01,
+#			"NodesMode": False,
+#    		"Vertices": [
+#        		{ "X": -0.4, "Y": 0.4012, "Z": 0.0 },
+#        		{ "X":  0.4, "Y":    0.4, "Z": 0.0 },
+#        		{ "X": -0.4, "Y":    0.4, "Z": 0.0 },
+#        		{ "X":  0.4, "Y":    0.4, "Z": 0.0 },
+#        		{ "X":  0.4, "Y": 0.4012, "Z": 0.0 },
+#        		{ "X": -0.4, "Y": 0.4012, "Z": 0.0 },
+#        		{ "X": -0.4, "Y": 0.3025, "Z": 0.0 },
+#        		{ "X":  0.4, "Y":    0.3, "Z": 0.0 },
+#        		{ "X": -0.4, "Y":    0.3, "Z": 0.0 },
+#        		{ "X":  0.4, "Y":    0.3, "Z": 0.0 },
+#        		{ "X":  0.4, "Y": 0.3025, "Z": 0.0 },
+#        		{ "X": -0.4, "Y": 0.3025, "Z": 0.0 },
+#        		{ "X": -0.4, "Y":  0.205, "Z": 0.0 },
+#        		{ "X":  0.4, "Y":    0.2, "Z": 0.0 },
+#        		{ "X": -0.4, "Y":    0.2, "Z": 0.0 },
+#        		{ "X":  0.4, "Y":    0.2, "Z": 0.0 },
+#        		{ "X":  0.4, "Y":  0.205, "Z": 0.0 },
+#        		{ "X": -0.4, "Y":  0.205, "Z": 0.0 },
+#        		{ "X":  0.4, "Y": 0.4006, "Z": 0.0 },
+#        		{ "X": -0.4, "Y": 0.4006, "Z": 0.0 },
+#        		{ "X":  0.4, "Y": 0.3012, "Z": 0.0 },
+#        		{ "X": -0.4, "Y": 0.3012, "Z": 0.0 },
+#        		{ "X":  0.4, "Y": 0.2025, "Z": 0.0 },
+#        		{ "X": -0.4, "Y": 0.2025, "Z": 0.0 } 
+#    		]
+#		}
+#	})
+#	return response
+#
+#def GetDocumentInfo(request):
+#	response = JsonResponse({
+#		"result": 0,
+#		"info": {
+#			"colorTheme": "dark",
+#			"thickness": "two",
+#			"nodeMode": "off",
+#			"objects": 123
+#		}
+#	})
+#	return response
+#
+#def getSceneData(request):
+#	filePath = os.path.join(os.getcwd(), 'data', 'data.json')
+#
+#	f = open(filePath, "r")
+#	jsonData = f.read()
+#	f.close()
+#
+#	if len(jsonData) != 0:
+#		data = json.loads(jsonData)
+#		vertices = data['vertices']
+#		indices = data['indices']
+#		counter = data['counter']
+#	else:
+#		vertices = []
+#		indices = []
+#		counter = 0
+#
+#	response = JsonResponse({
+#			'vertices': vertices,
+#			'indices': indices,
+#			'counter': counter
+#		})
+#
+#	return response
+#
 #@csrf_exempt
-def setSceneData(request):
-	if request.method == 'POST':
-		data = [ request.POST['vertices'], request.POST['indices'], request.POST['counter'] ]
-
-		vertices = [float(x) for x in data[0].split(',')]
-		indices = [int(x) for x in data[1].split(',')]
-		counter = int(data[2])
-
-		jsonData = json.dumps({ 'vertices': vertices, 'indices': indices, 'counter': counter},
-			indent = 4)
-		filePath = os.path.join(os.getcwd(), 'data', 'data.json')
-
-		f = open(filePath, "w")
-		f.write(jsonData)
-		f.close()
-
-	return HttpResponseRedirect(deploy['url'])
-
-def clearSceneData(request):
-	filePath = os.path.join(os.getcwd(), 'data', 'data.json')
-
-	f = open(filePath, "w")
-	f.write('')
-	f.close()
-
-	resetUserData()
-
-	return HttpResponseRedirect(deploy['url'])
-
-def getStatistics(request):
-	filePath = os.path.join(os.getcwd(), 'data', 'data.json')
-
-	f = open(filePath, "r")
-	jsonData = f.read()
-	f.close()
-
-	if len(jsonData) != 0:
-		data = json.loads(jsonData)
-		counter = data['counter']
-	else:
-		counter = 0
-
-	result = wrapper.statisticsWrapper(counter)
-
-	addUserData(result)
-
-	table = getUserData()
-	for line in table:
-		print(line)
-
-	response = JsonResponse({ 'objects': result })
-
-	return response
-# Old API #
+#def setSceneData(request):
+#	if request.method == 'POST':
+#		data = [ request.POST['vertices'], request.POST['indices'], request.POST['counter'] ]
+#
+#		vertices = [float(x) for x in data[0].split(',')]
+#		indices = [int(x) for x in data[1].split(',')]
+#		counter = int(data[2])
+#
+#		jsonData = json.dumps({ 'vertices': vertices, 'indices': indices, 'counter': counter},
+#			indent = 4)
+#		filePath = os.path.join(os.getcwd(), 'data', 'data.json')
+#
+#		f = open(filePath, "w")
+#		f.write(jsonData)
+#		f.close()
+#
+#	return HttpResponseRedirect(deploy['url'])
+#
+#def clearSceneData(request):
+#	filePath = os.path.join(os.getcwd(), 'data', 'data.json')
+#
+#	f = open(filePath, "w")
+#	f.write('')
+#	f.close()
+#
+#	resetUserData()
+#
+#	return HttpResponseRedirect(deploy['url'])
+#
+#def getStatistics(request):
+#	filePath = os.path.join(os.getcwd(), 'data', 'data.json')
+#
+#	f = open(filePath, "r")
+#	jsonData = f.read()
+#	f.close()
+#
+#	if len(jsonData) != 0:
+#		data = json.loads(jsonData)
+#		counter = data['counter']
+#	else:
+#		counter = 0
+#
+#	result = wrapper.statisticsWrapper(counter)
+#
+#	addUserData(result)
+#
+#	table = getUserData()
+#	for line in table:
+#		print(line)
+#
+#	response = JsonResponse({ 'objects': result })
+#
+#	return response
